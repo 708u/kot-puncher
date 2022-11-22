0500a927cbd2545e788cabb6085cd8a6c0a56682
708u <708.u.biz@gmail.com>
2022-11-22 19:00:37 +0900

    chore: nullish

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 export const ENV = {
-  KOT_URL: Deno.env.get('KOT_URL') || '',
-  KOT_USER_ID: Deno.env.get('KOT_USER_ID') || '',
-  KOT_USER_PASSWORD: Deno.env.get('KOT_USER_PASSWORD') || '',
-  SLACK_WEBHOOK_URL: Deno.env.get('KOT_PUNCHER_SLACK_WEBHOOK_URL') || '',
+  KOT_URL: Deno.env.get('KOT_URL') ?? '',
+  KOT_USER_ID: Deno.env.get('KOT_USER_ID') ?? '',
+  KOT_USER_PASSWORD: Deno.env.get('KOT_USER_PASSWORD') ?? '',
+  SLACK_WEBHOOK_URL: Deno.env.get('KOT_PUNCHER_SLACK_WEBHOOK_URL') ?? '',
 } as const
