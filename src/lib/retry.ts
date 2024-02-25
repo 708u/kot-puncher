377b28e8ae42d71d245b23c128b0bd065594f39f
708u <708.u.biz@gmail.com>
2024-02-25 11:40:03 +0900

    feat: add backoff to retry

--- src/lib/retry.ts
+++ src/lib/retry.ts
@@ -1,12 +1,19 @@
-export const retryAsync = async <TResult>(func: () => Promise<TResult>, attempts?: number): Promise<TResult> => {
+export const retryAsync = async <TResult>(
+  func: () => Promise<TResult>,
+  attempts?: number,
+  backoff?: number
+): Promise<TResult> => {
   let count = 0
   attempts ??= 3
+  backoff ??= 1000
   while (count < attempts) {
     try {
       return await func()
     } catch (e) {
-      console.error(`retry ${count + 1} failed. ${e}`)
+      console.error(`retry ${count + 1} failed. ${e}. retrying in ${backoff}ms.`)
       count++
+      backoff *= 2
+      await new Promise(resolve => setTimeout(resolve, backoff))
     }
   }
   throw new Error('retry failed')
